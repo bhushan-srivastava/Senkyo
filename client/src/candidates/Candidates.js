b2fedaeba16b46d74d120377f0174aa097e7ddd7
Bhushan Srivastava <84985241+bhushan-srivastava@users.noreply.github.com>
2024-03-06 23:04:50 +0530

    Delete client directory

--- client/src/candidates/Candidates.js
+++
@@ -1,5 +0,0 @@
-const Candidates = () => {
-    return ("candidates panel");
-}
-
-export default Candidates;
--- client/src/static/pages/Welcome.js
+++
@@ -1,5 +0,0 @@
-const Welcome = () => {
-    return ("landing page");
-}
-
-export default Welcome;
